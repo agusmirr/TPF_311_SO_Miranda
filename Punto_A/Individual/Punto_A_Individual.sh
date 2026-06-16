a4a0a9d398070201814b467bb1b17c372760c388
Agustina Miranda <agusmirr@gmail.com>
2026-06-16 16:03:48 +0000

    Manager: agrego script individual del Punto A

--- Punto_A/Individual/Punto_A_Individual.sh
+++ Punto_A/Individual/Punto_A_Individual.sh
@@ -10,3 +10,14 @@ touch ~/sysadmin/Servicios/Cache/.gitkeep
 touch ~/sysadmin/Monitoreo/Logs/.gitkeep
 touch ~/sysadmin/Monitoreo/Alertas/.gitkeep
 touch ~/sysadmin/Monitoreo/Metricas/.gitkeep
+
+# Manager - estructura individual
+mkdir -p ~/manager/{Servicios/{Web,Base_de_Datos,Cache},Monitoreo/{Logs,Alertas,Metricas}}
+
+# Archivos .gitkeep para evitar directorios hoja vacíos
+touch ~/manager/Servicios/Web/.gitkeep
+touch ~/manager/Servicios/Base_de_Datos/.gitkeep
+touch ~/manager/Servicios/Cache/.gitkeep
+touch ~/manager/Monitoreo/Logs/.gitkeep
+touch ~/manager/Monitoreo/Alertas/.gitkeep
+touch ~/manager/Monitoreo/Metricas/.gitkeep
