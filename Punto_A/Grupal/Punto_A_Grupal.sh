3b9635a6d5b7ebbf25a9bd398a41dcffa1ac6b96
Agustina Miranda <agusmirr@gmail.com>
2026-06-16 16:15:36 +0000

    Manager: agrego estructura grupal Europa del Punto A

--- Punto_A/Grupal/Punto_A_Grupal.sh
+++ Punto_A/Grupal/Punto_A_Grupal.sh
@@ -12,3 +12,16 @@ sudo touch /Continentes/America/Uruguay/Salto/.gitkeep
 sudo touch /Continentes/America/Peru/Lima/.gitkeep
 sudo touch /Continentes/America/Peru/Cusco/.gitkeep
 sudo touch /Continentes/America/Mexico/CDMX/.gitkeep
+
+# Manager - Europa
+sudo mkdir -p /Continentes/Europa/España/{Madrid,Barcelona,Toledo}
+sudo mkdir -p /Continentes/Europa/Italia/{Turin,Napoles}
+sudo mkdir -p /Continentes/Europa/Francia/Marsella
+
+# Archivos para evitar directorios hoja vacíos
+sudo touch /Continentes/Europa/España/Madrid/.gitkeep
+sudo touch /Continentes/Europa/España/Barcelona/.gitkeep
+sudo touch /Continentes/Europa/España/Toledo/.gitkeep
+sudo touch /Continentes/Europa/Italia/Turin/.gitkeep
+sudo touch /Continentes/Europa/Italia/Napoles/.gitkeep
+sudo touch /Continentes/Europa/Francia/Marsella/.gitkeep
