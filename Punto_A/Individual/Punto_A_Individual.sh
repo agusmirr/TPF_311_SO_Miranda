4ab8f7c2584d1a6735ec8e3ddb17751e91ba9495
Agustina Miranda <agusmirr@gmail.com>
2026-06-16 16:58:48 +0000

    Dev: agrego script individual del Punto A

--- Punto_A/Individual/Punto_A_Individual.sh
+++ Punto_A/Individual/Punto_A_Individual.sh
@@ -21,3 +21,14 @@ touch ~/manager/Servicios/Cache/.gitkeep
 touch ~/manager/Monitoreo/Logs/.gitkeep
 touch ~/manager/Monitoreo/Alertas/.gitkeep
 touch ~/manager/Monitoreo/Metricas/.gitkeep
+
+# Dev - estructura individual
+mkdir -p ~/dev/{Servicios/{Web,Base_de_Datos,Cache},Monitoreo/{Logs,Alertas,Metricas}}
+
+# Archivos .gitkeep para evitar directorios hoja vacíos
+touch ~/dev/Servicios/Web/.gitkeep
+touch ~/dev/Servicios/Base_de_Datos/.gitkeep
+touch ~/dev/Servicios/Cache/.gitkeep
+touch ~/dev/Monitoreo/Logs/.gitkeep
+touch ~/dev/Monitoreo/Alertas/.gitkeep
+touch ~/dev/Monitoreo/Metricas/.gitkeep
