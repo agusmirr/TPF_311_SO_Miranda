4d5143a3ea9c7d52da455e20ba60f696fa793780
Agustina Miranda <agusmirr@gmail.com>
2026-06-16 17:03:58 +0000

    Dev: agrego estructura grupal Asia del Punto A

--- Punto_A/Grupal/Punto_A_Grupal.sh
+++ Punto_A/Grupal/Punto_A_Grupal.sh
@@ -25,3 +25,16 @@ sudo touch /Continentes/Europa/España/Toledo/.gitkeep
 sudo touch /Continentes/Europa/Italia/Turin/.gitkeep
 sudo touch /Continentes/Europa/Italia/Napoles/.gitkeep
 sudo touch /Continentes/Europa/Francia/Marsella/.gitkeep
+
+# Dev - Asia
+sudo mkdir -p /Continentes/Asia/China/{Beijing,Shanghai,Shenzhen}
+sudo mkdir -p /Continentes/Asia/Japon/{Tokyo,Osaka}
+sudo mkdir -p /Continentes/Asia/Tailandia/Bangkok
+
+# Archivos para evitar directorios hoja vacíos
+sudo touch /Continentes/Asia/China/Beijing/.gitkeep
+sudo touch /Continentes/Asia/China/Shanghai/.gitkeep
+sudo touch /Continentes/Asia/China/Shenzhen/.gitkeep
+sudo touch /Continentes/Asia/Japon/Tokyo/.gitkeep
+sudo touch /Continentes/Asia/Japon/Osaka/.gitkeep
+sudo touch /Continentes/Asia/Tailandia/Bangkok/.gitkeep
