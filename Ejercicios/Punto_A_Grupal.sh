e2413b70ce6b239bc3fcb85a085fc0d4df0380f4
Agustina Miranda <agusmirr@gmail.com>
2026-06-26 16:03:55 +0000

    Sysadmin: agrego script Punto_A_Grupal.sh en Ejercicios y estructura de America en Grupal/Continentes

---
+++ Ejercicios/Punto_A_Grupal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Rol: sysadmin
+# Continente: America
+# Crea la estructura de America dentro de /Grupal/Continentes/
+mkdir -p /Grupal/Continentes/America/{Mexico/{Guadalajara,Monterrey},Colombia/Medellin,Peru/{Cusco,Arequipa,Trujillo}}
+touch /Grupal/Continentes/America/{Mexico/{Guadalajara,Monterrey},Colombia/Medellin,Peru/{Cusco,Arequipa,Trujillo}}/.gitkeep
