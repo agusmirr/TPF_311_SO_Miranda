8e46397103959e963dc20d65448f514a4e77a236
Agustina Miranda <agusmirr@gmail.com>
2026-06-26 16:42:30 +0000

    Dev: agrego estructura de Asia en Grupal/Continentes y unifico script Punto_A_Grupal.sh con los tres roles

--- Ejercicios/Punto_A_Grupal_manager.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Rol: manager
-# Continente: Europa
-# Crea la estructura de Europa dentro de /Grupal/Continentes/
-mkdir -p /Grupal/Continentes/Europa/{Francia/{Lyon,Marsella},Croacia/Dubrovnik,España/{Sevilla,Valencia,Bilbao}}
-touch /Grupal/Continentes/Europa/{Francia/{Lyon,Marsella},Croacia/Dubrovnik,España/{Sevilla,Valencia,Bilbao}}/.gitkeep
